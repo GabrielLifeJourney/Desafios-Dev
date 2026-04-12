a6d72cc9fcc933df9d03dd5731b40f55673c8454
GabrielLifeJourney <gabriellifejourney@gmail.com>
2026-04-12 20:55:35 -0300

    bug

---
+++ Desafio+Master/modulo-4/d040/php/login.php
@@ -0,0 +1,4 @@
+Mensagem
+
+Atualmente estou focado em aprender HTML e CSS, construindo uma base sólida no desenvolvimento web.
+Daqui a alguns meses, vou evoluir para o PHP, dando mais um passo importante rumo ao back-end.
